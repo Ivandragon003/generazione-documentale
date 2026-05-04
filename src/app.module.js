4d956508a54bb8704506be388fad89f1c9aeeea5
Ivan De Felice <ivandefelice05@gmail.com>
2026-05-04 15:53:21 +0200

    feat: esclude DevModule (e le sue route Swagger) in NODE_ENV=production

--- src/app.module.js
+++ src/app.module.js
@@ -5,12 +5,18 @@ const { HealthModule }    = require('./modules/health/health.module');
 const { AuditModule }     = require('./modules/audit/audit.module');
 const { TemplatesModule } = require('./modules/templates/template.module');
 const { DocumentsModule } = require('./modules/documents/document.module');
-const { DevModule }       = require('./modules/dev/dev.module');
+
+// DevModule registrato solo in sviluppo:
+// - le route /api/dev/* non esistono in produzione
+// - la sezione "dev" sparisce completamente dallo Swagger
+const devImports = process.env.NODE_ENV !== 'production'
+  ? [require('./modules/dev/dev.module').DevModule]
+  : [];
 
 class AppModule {}
 
 Module({
-  imports: [HealthModule, AuditModule, TemplatesModule, DocumentsModule, DevModule],
+  imports: [HealthModule, AuditModule, TemplatesModule, DocumentsModule, ...devImports],
 })(AppModule);
 
 module.exports = { AppModule };
