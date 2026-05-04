34d6e6e6355c301c10d4f1a1a90366057174df81
Ivan De Felice <ivandefelice05@gmail.com>
2026-05-04 11:51:10 +0200

    fix(mock-server): writer.js risponde 200 con {} quando il service ritorna undefined

--- mock-server/utils/writer.js
+++ mock-server/utils/writer.js
@@ -20,24 +20,21 @@ var writeJson = exports.writeJson = function(response, arg1, arg2) {
 
   if (arg2 && Number.isInteger(arg2)) {
     code = arg2;
-  } else {
-    if (arg1 && Number.isInteger(arg1)) {
-      code = arg1;
-    }
-  }
-
-  if (code && arg1) {
-    payload = arg1;
-  } else if (arg1) {
-    payload = arg1;
+  } else if (arg1 && Number.isInteger(arg1)) {
+    code = arg1;
   }
 
   if (!code) {
     code = 200;
   }
 
-  if (typeof payload === 'object') {
-    payload = JSON.stringify(payload, null, 2);
+  // Se il service non restituisce nulla (undefined/null), rispondi con oggetto vuoto
+  if (arg1 === undefined || arg1 === null || Number.isInteger(arg1)) {
+    payload = '{}';
+  } else if (typeof arg1 === 'object') {
+    payload = JSON.stringify(arg1, null, 2);
+  } else {
+    payload = arg1;
   }
 
   response.writeHead(code, { 'Content-Type': 'application/json' });
