aac6495eab9aa68b38b858d0183d6676d3eedd01
Ivan De Felice <ivandefelice05@gmail.com>
2026-05-06 10:35:22 +0200

    refactor: remove dev module - dev.module.ts

--- src/modules/dev/module/dev.module.ts
+++
@@ -1,9 +0,0 @@
-﻿import { Module } from "@nestjs/common";
-import { DevController } from "../controller/dev.controller";
-import { DevRepository } from "../repository/dev.repository";
-
-@Module({
-  controllers: [DevController],
-  providers: [DevRepository],
-})
-export class DevModule {}
