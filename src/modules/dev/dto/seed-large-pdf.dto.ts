04820cecb1a15e9021283c0ae4e04646c191ae62
Ivan De Felice <ivandefelice05@gmail.com>
2026-05-06 10:36:05 +0200

    refactor: remove dev module - seed-large-pdf.dto.ts

--- src/modules/dev/dto/seed-large-pdf.dto.ts
+++
@@ -1,9 +0,0 @@
-﻿import { ApiPropertyOptional } from "@nestjs/swagger";
-import { IsOptional, IsString } from "class-validator";
-
-export class SeedLargePdfDto {
-  @ApiPropertyOptional({ example: "http://localhost:3000" })
-  @IsOptional()
-  @IsString()
-  baseUrl?: string;
-}
